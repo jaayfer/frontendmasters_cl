2260856e5a8317d9d716653d6e589037ed318942
jaayfer <bjaayfer@gmail.com>
2020-09-18 12:50:41 +0100

    fixing issues responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuNav = document.querySelector(".menu-nav");
 const hamburger = document.querySelector(".menu-btn__ham");
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let menuOpen = false;
 
@@ -9,11 +10,13 @@ menuBtn.addEventListener('click',(e)=>{
     if(menuOpen){
         menuNav.classList.remove("open");
         hamburger.classList.remove("open")
+        navItems.forEach(item=> item.classList.remove("open"))
         menuOpen = false;
     }
     else{
         menuNav.classList.add("open");
         hamburger.classList.add("open");
+        navItems.forEach(item=> item.classList.add("open"))
         menuOpen = true;
     }
    
